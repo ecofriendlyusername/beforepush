befd287e92eebf4895ca36f24b80e623ace7c4bd
No Name <gayoung.yang.0@gmail.com>
2022-03-26 06:35:53 -0400

    post controller

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,11 +1,5 @@
 package com.example.demo.controllers;
 
-import java.util.Collections;
-import java.util.Map;
-
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
